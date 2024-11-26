a22f4a65a36012883dcd47aa8ea996da9f75181c
Mahir <mahir.suljic@fet.ba>
2024-11-26 19:16:07 +0100

    ExampleSolution

--- Predavanja/ExampleSolution/app/obj/Debug/net8.0/app.AssemblyInfo.fs
+++ Predavanja/ExampleSolution/app/obj/Debug/net8.0/app.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("app")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+922fc0cf71fe09983a9ca07d04ec321a954621c5")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+971f561755d5be55f409e905003692920e5609d2")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("app")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("app")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
